37bbec7a7c0241b305a5af950adf8a86186d69dc
meetghodasara <meet.d.ghodasara@gmail.com>
2024-08-15 22:35:26 +0530

    Initial project setup with basic structure and dependencies.

---
+++ fooddeliveryapp/src/main/java/com/lamda/fooddeliveryapp/FoodDeliveryApp.java
@@ -0,0 +1,13 @@
+package com.lamda.fooddeliveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApp.class, args);
+	}
+
+}
---
+++ fooddeliveryapp/src/test/java/com/lamda/fooddeliveryapp/FoodDeliveryAppTests.java
@@ -0,0 +1,13 @@
+package com.lamda.fooddeliveryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
